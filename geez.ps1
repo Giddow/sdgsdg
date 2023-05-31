2bba1b0001aac69480d3b9647ef7fdc724f49fa9
Giddow <106636675+Giddow@users.noreply.github.com>
2023-05-31 12:02:27 +0100

    Update geez.ps1

--- geez.ps1
+++ geez.ps1
@@ -1,6 +1,6 @@
 ﻿Set-ExecutionPolicy Bypass -Scope Process -Force
 #$source = 'https://github.com/Giddow/sdgsdg/raw/main/Desktop%20Goose.zip'
 #$destination = "$env:userprofile\Downloads\yeettt.zip"
-Invoke-RestMethod -Uri 'https://github.com/Giddow/sdgsdg/raw/main/DesktopGoose.zip' -OutFile $env:userprofile\\Downloads\\goosi.zip
-Expand-Archive $env:userprofile\\Downloads\\goosi.zip -DestinationPath $env:userprofile -Force
-powershell \"$env:userprofile\\DesktopGoose\\bat.ps1\"
+Invoke-RestMethod -Uri 'https://github.com/Giddow/sdgsdg/raw/main/DesktopGoose.zip' -OutFile $env:userprofile\Downloads\goosi.zip
+Expand-Archive "$env:userprofile\Downloads\goosi.zip" -DestinationPath $env:userprofile -Force
+powershell "$env:userprofile\DesktopGoose\bat.ps1"
